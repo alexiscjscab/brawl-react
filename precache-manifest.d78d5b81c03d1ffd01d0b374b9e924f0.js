1da1c7cbf1bcf6dc245c02580e816f3f548d7ef8
alexiscjscab <alexiscjscab@gmail.com>
2022-04-09 17:28:12 -0300

    Updates

---
+++ precache-manifest.d78d5b81c03d1ffd01d0b374b9e924f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "086a96c4026a7428bd7a",
+    "url": "/brawl-react/static/css/main.8380f5ca.chunk.css"
+  },
+  {
+    "revision": "086a96c4026a7428bd7a",
+    "url": "/brawl-react/static/js/main.086a96c4.chunk.js"
+  },
+  {
+    "revision": "d84f6d625b9b475443fa",
+    "url": "/brawl-react/static/js/1.d84f6d62.chunk.js"
+  },
+  {
+    "revision": "f161cc52c6bd1fb7b2b0",
+    "url": "/brawl-react/static/js/runtime~main.f161cc52.js"
+  },
+  {
+    "revision": "0b5791597ffcff4ebd4c5b5c7bb75d71",
+    "url": "/brawl-react/index.html"
+  }
+];
